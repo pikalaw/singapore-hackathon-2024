18fb55fec723e8c63c8c66c24f83f77f38848b0d
Lawrence Tsang <pikalaw@gmail.com>
2024-06-26 11:42:51 -0400

    Added timezone to current_datetime tool

--- functions/real_time.py
+++ functions/real_time.py
@@ -1,12 +1,20 @@
-from datetime import datetime
+"""A collection of functions related to real-time information."""
+
+import datetime
+import zoneinfo
 import logging
 
 
-async def current_datetime() -> str:
+async def current_datetime(timezone: str = "America/New_York") -> str:
     """Returns the current date and time.
+
+    Args:
+        timezone: The timezone to use.
 
     Returns:
         The current date and time.
     """
-    logging.info("Getting the current date and time.")
-    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    logging.info("Getting the current date and time in %s.", timezone)
+    tz = zoneinfo.ZoneInfo(timezone)
+
+    return datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
